dba1675965d97f2ed4f64dd5e37686a1bd978ecd
git1975 <git1975>
2015-11-20 09:10:43 +0300

    1

--- testbot/web/run-hook.php
+++ testbot/web/run-hook.php
@@ -5,7 +5,7 @@ define('WEBHOOK_URL', 'https://dztestbot.herokuapp.com/web/index.php');
 
 //if (php_sapi_name() == 'cli') {
   // if run from console, set or delete webhook
-  apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
+  apiRequest('setWebhook', WEBHOOK_URL);
 //  exit;
 //}
 echo "setWebhook OK";
---
+++ testbot/web/stop-hook.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'index.php';
+
+define('WEBHOOK_URL', 'https://dztestbot.herokuapp.com/web/index.php');
+
+apiRequest('setWebhook', '');
+
+echo "setWebhook Stoped";
