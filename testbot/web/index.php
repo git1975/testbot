0d1c02be8783f0efea16a2b83bfcf4a0d8c98614
git1975 <git1975>
2015-11-20 09:39:58 +0300

    1

--- testbot/web/index.php
+++ testbot/web/index.php
@@ -141,13 +141,13 @@ function processMessage($message) {
   }
 }
 
-/*define('WEBHOOK_URL', 'https://dztestbot.herokuapp.com/web/index.php');
+define('WEBHOOK_URL', 'https://dztestbot.herokuapp.com/web/index.php');
 
 if (php_sapi_name() == 'cli') {
   // if run from console, set or delete webhook
   apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
   exit;
-}*/
+}
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
