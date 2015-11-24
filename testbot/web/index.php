ec0a9797a7f5b79fcce2257b22d7480db0386d6c
git1975 <git1975>
2015-11-24 09:00:24 +0300

    1

--- testbot/web/index.php
+++ testbot/web/index.php
@@ -123,21 +123,21 @@ function processMessage($message) {
     $text = $message['text'];
 
     if (strpos($text, "/start") === 0) {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
+      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Привет', 'reply_markup' => array(
         'keyboard' => array(array('Hello', 'Hi')),
         'one_time_keyboard' => true,
         'resize_keyboard' => true)));
-    } else if ($text === "Hello" || $text === "Hi") {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
-    } else if ($text === "Bye") {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Bye-bye!'));
+    } else if (strpos($text, "Привет") === 0) {
+      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Рад Вас видеть!'));
+    } else if ($text === "Пока") {
+      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Пока-пока!'));
     } else if (strpos($text, "/stop") === 0) {
       // stop now
     } else {
       apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Cool'));
     }
   } else {
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
+    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Я понимаю только текст'));
   }
 }
 
