c4534f1517e0178674514dd482b4c0961969c836
git1975 <git1975>
2015-12-04 08:43:50 +0300

    1

--- testbot/web/index.php
+++ testbot/web/index.php
@@ -213,9 +213,11 @@ define('WEBHOOK_URL', 'https://alfaprofitbot.herokuapp.com/web/index.php');
 
 //if (php_sapi_name() == 'cli') {
 // if run from console, set or delete webhook
-apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
+//apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
 //exit;
 //}
+
+apiRequest('setWebhook', array('url' => ''));
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
