aa67d87696f7d862f50ac3cdc91ca218b19c0fa2
git1975 <git1975>
2015-12-04 10:11:19 +0300

    1

--- testbot/web/common.php
+++ testbot/web/common.php
@@ -132,7 +132,7 @@ function processMessage($message) {
     if (isset($message['text'])) {
         // incoming text message
         $text = $message['text'];
-        $text = mb_strtolower($text);
+        //$text = strtolower($text);
         error_log("chat_id: $chat_id");
         error_log("INCOMING MESSAGE: $text");
 
@@ -171,7 +171,7 @@ function processMessage($message) {
         	$file = 'user1.txt';
         	file_put_contents($file, $chat_id);
         	apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Ок, я запомнил!'));
-        } else if (mb_strtolower($text) === mb_strtolower("я заемщик")) {
+        } else if (strtolower($text, 'utf8') === strtolower("я заемщик", 'utf8')) {
         		$file = 'user2.txt';
         		file_put_contents($file, $chat_id);
         		apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Ок, я запомнил!'));
