7343d7984f649590d57a51f58ab866eaf479bc7f
gmananton <g-man2006@yandex.ru>
2015-12-04 23:53:12 +0300

    1

---
+++ testbot/web/paymentLogic.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: gmananton
+ * Date: 04.12.15
+ * Time: 23:49
+ * класс с логикой для рассчета графика платежей
+ */
+class PaymentLogic {
+
+    
+
+}
