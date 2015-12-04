a93fc08a67948fca41fadab7c29d7943e44a239d
gmananton <g-man2006@yandex.ru>
2015-12-04 19:54:35 +0300

    1

--- testbot/web/baseHandler.php
+++ testbot/web/baseHandler.php
@@ -9,14 +9,14 @@ require_once 'actionNoCard.php';
 require_once 'telegram_io.php';
 
 class BaseHandler{
-	protected $message;
-	protected $text;
-	protected $keyboard;
-	protected $msgStart;
-	protected $msgBorrow;
-	protected $msgLend;
-	protected $message_id;
-	protected $chat_id;
+	public $message;
+    public $text;
+    public $keyboard;
+    public $msgStart;
+    public $msgBorrow;
+    public $msgLend;
+    public $message_id;
+    public $chat_id;
 
 	function init() {
 		$keyboards = new Keyboards;
@@ -25,6 +25,7 @@ class BaseHandler{
 		$msgLend = new MessagesLend;
 		$message_id = $this->message['message_id'];
 		$chat_id = $this->message['chat']['id'];
+        error_log("------>Method init() message: $this->message chat_id=$this->chat_id");
 		$text = $this->message['text'];
 	}
 }
