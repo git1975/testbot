244af723ea29a0daad26112daa2bb7362e03bd07
gmananton <g-man2006@yandex.ru>
2015-12-05 02:15:20 +0300

    1

--- testbot/web/paymentLogic.php
+++ testbot/web/paymentLogic.php
@@ -28,7 +28,7 @@ class PaymentLogic {
 
     private function getNextPaymentDate($referenceDate){
         // referenceDate - стартовая дата. Текущая, либо каждая следующая на каждом шаге итерации
-        //$date = new DateTime("dd.mm.YY");
+
         $newDate = $referenceDate->modify("+1 month");
         error_log("Date in a month: " + $newDate->format("Y-m-d"));
         return $newDate->format("Y-m-d");
@@ -37,7 +37,7 @@ class PaymentLogic {
     public function getPaymentSchedule($period) {
         /* получает на вход кол-во месяцев, на выходе - массив дат с шагом в месяц */
         $datesArray = [];
-        $referenceDate = new DateTime("Y-m-d"); //текущая дата
+        $referenceDate = new DateTime(); //текущая дата
         for ($i = 0; $i < $period; $i++ ) {
             $datesArray[i] = $this->getNextPaymentDate($referenceDate);
             $referenceDate = $this->getNextPaymentDate($referenceDate);
