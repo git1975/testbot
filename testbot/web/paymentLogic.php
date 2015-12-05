649a31e32061b42afbe0f97279ce5e53af1b9a81
gmananton <g-man2006@yandex.ru>
2015-12-05 09:22:52 +0300

    1

--- testbot/web/paymentLogic.php
+++ testbot/web/paymentLogic.php
@@ -41,8 +41,7 @@ class PaymentLogic {
         $referenceDate = date('Y-m-d');
 
         for ($i = 0; $i < $period; $i++ ) {
-            error_log("I=$i");
-            $datesArray[i] = $this->getNextPaymentDate($referenceDate);
+            $datesArray[$i] = $this->getNextPaymentDate($referenceDate);
             $referenceDate = $this->getNextPaymentDate($referenceDate);
             error_log("REFERENCE DATE: $referenceDate"); //текущая дата
         }
