cdbe25ff67055116c2957ec8981fef9a0ebb1339
git1975 <denzorin@yahoo.com>
2015-12-05 09:32:31 +0300

    Update paymentLogic.php

--- testbot/web/paymentLogic.php
+++ testbot/web/paymentLogic.php
@@ -42,7 +42,7 @@ class PaymentLogic {
 
         for ($i = 0; $i < $period; $i++ ) {
             error_log("I=$i");
-            $datesArray[i] = $this->getNextPaymentDate($referenceDate);
+            $datesArray[$i] = $this->getNextPaymentDate($referenceDate);
             $referenceDate = $this->getNextPaymentDate($referenceDate);
             error_log("REFERENCE DATE: $referenceDate"); //текущая дата
         }
